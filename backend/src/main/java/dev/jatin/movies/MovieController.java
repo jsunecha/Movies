80f67f2157837cee99f3451b4082d7348e1706ae
Jatin Unecha <jatinunecha@gmail.com>
2023-06-12 17:53:10 -0700

    edited backend files to enable CORS

--- backend/src/main/java/dev/jatin/movies/MovieController.java
+++ backend/src/main/java/dev/jatin/movies/MovieController.java
@@ -4,25 +4,25 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
+@CrossOrigin(origins = "*")
 public class MovieController {
     @Autowired
     private MovieService movieService;
 
+    @CrossOrigin
     @GetMapping
     public ResponseEntity<List<Movie>> getAllMovies(){
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
 
+    @CrossOrigin
     @GetMapping("/{id}")
     public ResponseEntity<Optional<Movie>> getMovie(@PathVariable String id){
         return new ResponseEntity<Optional<Movie>>(movieService.SingleMovie(id), HttpStatus.OK);
